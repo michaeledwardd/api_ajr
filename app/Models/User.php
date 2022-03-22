dbdb0a2778522f04f7536fae8103216b64a73e51
user <michedwards0@gmail.com>
2022-03-22 22:31:17 +0700

    tambahan v01

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+// use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
+use Carbon\Carbon;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getCreatedAttribute()
+    {
+        if(!is_null($this->attributes['created_at']))
+        {
+            return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+        }
+    }
+
+    public function getUpdatedAttribute()
+    {
+        if(!is_null($this->attributes['updated_at']))
+        {
+            return Carbon::parse($this->attributes['update_at'])->format('Y-m-d H:i:s');
+        }
+    }
 }
