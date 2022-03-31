ef789ed96b3751c1b275fc891184e703aa52fe97
user <michedwards0@gmail.com>
2022-03-31 21:42:34 +0700

    cruds 10 table fix

--- app/Http/Controllers/Api/TransaksiController.php
+++ app/Http/Controllers/Api/TransaksiController.php
@@ -49,16 +49,16 @@ class TransaksiController extends Controller
     public function store(Request $request){
         $storeData = $request->all(); //Mengambil semua input dari API Client
         $validate = Validator::make($storeData, [
-            'id_customer' => 'required|numeric',
+            'id_customer' => 'required',
             'id_mobil' => 'required|numeric',
             'id_pegawai' => 'required|numeric',
-            'id_driver' => 'required|numeric',
-            'id_promo' => 'required|numeric',
+            'id_driver',
+            'id_promo'=>'numeric',
             'tgl_transaksi' => 'required|date_format:Y-m-d',
             'tgl_pinjam' => 'required|date_format:Y-m-d',
             'tgl_kembali' => 'required|date_format:Y-m-d|after:tgl_pinjam',
             'tgl_selesai_pinjam' => 'required|date_format:Y-m-d|after:tgl_pinjam',
-            'jenis_peminjaman' => 'required|regex:/^[\pL\s\-]+$/u',
+            'jenis_peminjaman',
             'cek_terlambat' =>'required',
             'total_denda' => 'required|numeric',
             'total_biaya_pinjam' => 'required|numeric',
@@ -76,14 +76,43 @@ class TransaksiController extends Controller
             return response(['message' => $validate->errors()], 400); //Return error invalid input
         }
 
-        $count= DB::table('driver')->count() +1;
+        $count= DB::table('transaksi')->count() +1;
         $id_generate = sprintf("%03d", $count);
         $datenow = Carbon::now()->format('dmy');
 
+        if(($request->id_driver)===NULL)
+        {
+            $kode_pinjam = sprintf("02");
+            $request->jenis_peminjaman = sprintf("mobil");
+        }
+        else{
+            $kode_pinjam = sprintf("01");
+            $request->jenis_peminjaman = sprintf("mobil + driver");
+        }
+
         $Transaksi = Transaksi::create([
-            'id_transaksi'=>'TRN'.$datenow.$id_generate,
-            '',
-            '',
+            'id_transaksi'=>'TRN'.$datenow.$kode_pinjam.'-'.$id_generate,
+            'id_customer'=>$request->id_customer,
+            'id_mobil'=>$request->id_mobil,
+            'id_pegawai'=>$request->id_pegawai,
+            'id_driver'=>$request->id_driver,
+            'id_promo'=>$request->id_promo,
+            'tgl_transaksi'=>$request->tgl_transaksi,
+            'tgl_pinjam'=>$request->tgl_pinjam,
+            'tgl_kembali'=>$request->tgl_kembali,
+            'tgl_selesai_pinjam'=>$request->tgl_selesai_pinjam,
+            'jenis_peminjaman'=>$request->jenis_peminjaman,
+            'cek_terlambat'=>$request->cek_terlambat,
+            'total_denda'=>$request->total_denda,
+            'total_biaya_pinjam'=>$request->total_biaya_pinjam,
+            'biaya_denda'=>$request->biaya_denda,
+            'total_sewa_driver'=>$request->total_sewa_driver,
+            'bukti_bayar'=>$request->bukti_bayar,
+            'subtotal_all'=>$request->subtotal_all,
+            'status_transaksi'=>$request->status_transaksi,
+            'metode_bayar'=>$request->metode_bayar,
+            'rating_perform_driver'=>$request->rating_perform_driver,
+            'rating_perform_ajr'=>$request->rating_perform_ajr
         ]);
 
         return response([
@@ -129,10 +158,27 @@ class TransaksiController extends Controller
 
         $updateData = $request->all();
         $validate = Validator::make($updateData, [
-            'hari_kerja' => 'required|regex:/^[\pL\s\-]+$/u',
-            'jenis_shift' => 'required|numeric',
-            'jam_mulai' => 'required|date_format:H:i',
-            'jam_selesai' => 'required|date_format:H:i|after:jam_mulai'
+            'id_customer' => 'required',
+            'id_mobil' => 'required|numeric',
+            'id_pegawai' => 'required|numeric',
+            'id_driver',
+            'id_promo'=>'numeric',
+            'tgl_transaksi' => 'required|date_format:Y-m-d',
+            'tgl_pinjam' => 'required|date_format:Y-m-d',
+            'tgl_kembali' => 'required|date_format:Y-m-d|after:tgl_pinjam',
+            'tgl_selesai_pinjam' => 'required|date_format:Y-m-d|after:tgl_pinjam',
+            'jenis_peminjaman',
+            'cek_terlambat' =>'required',
+            'total_denda' => 'required|numeric',
+            'total_biaya_pinjam' => 'required|numeric',
+            'biaya_denda' => 'required|numeric',
+            'total_sewa_driver' => 'required|numeric',
+            'bukti_bayar' => 'required',
+            'subtotal_all' => 'required|numeric',
+            'status_transaksi' => 'required|regex:/^[\pL\s\-]+$/u',
+            'metode_bayar' => 'required|regex:/^[\pL\s\-]+$/u',
+            'rating_perform_driver' => 'required|numeric',
+            'rating_perform_ajr' => 'required|numeric'
         ]); //Membuat rule validasi input
 
         if($validate->fails()){
